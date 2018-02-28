29f8b088dbd654fd46eb88c1f4ac24e4c57a411a
世纪 <meishijiemeimei@163.com>
2018-02-28 16:43:00 +0800

    1
    1

--- pinyin.py
+++ pinyin.py
@@ -5,9 +5,15 @@ from pypinyin import pinyin, lazy_pinyin, Style
 # print (pinyin(u'蹒跚',style=Style.FINALS))
 import sqlite3
 
+# 兼容python2.7
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+# 
+
 def search(temp):
-	search_temp = temp.decode('utf-8')
-	search_pinyin = pinyin(search_temp,style=Style.FINALS)
+	# search_temp = temp.decode('UTF-8')
+	search_pinyin = pinyin(temp,style=Style.FINALS)
 	# print search_pinyin
 	conn = sqlite3.connect('c.db')
 	c = conn.cursor()
@@ -22,10 +28,10 @@ def search(temp):
 	    	item_temp = item[-2:]
 	    	pinyin_temp = pinyin(item,style=Style.FINALS)[-2:]
 	    	if(search_pinyin == pinyin_temp):
-	    		print u'歌曲:',lyc_name
-	    		print item
-	    		print '-----'
+	    		print (u'歌曲:'+lyc_name)
+	    		print (item)
+	    		print ('-----')
 	conn.close()
 # 
-search('意思')
+search('意义')
 
