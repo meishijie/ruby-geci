255c19625226cfa601c505cefae5ce59c7ea1172
meishijie <719082908@qq.com>
2018-03-01 20:55:42 +0800

    1
    1

--- test多线程.rb
+++ test多线程.rb
@@ -8,13 +8,32 @@ require 'timeout'
 require 'enumerator'
 
 
+  @nowarray  = []
+  @alllist = []
+  @database = 1
+  $queue = []
+  @data={}
+
+  def insert(i)
+    # 转成字符串存入数据库
+    @data["_lrcname"] = '1'
+    @data["_album"] = '1'
+    @data["_albumLink"] = '1'
+    @data["_artist"] = '1'
+    @data["_artistLink"] = '1'
+    @data["_lyccontent"] = i
+    # 根据@database的名字不同 存入不同的数据库
+    SQLite3::Database.new("test.db") do |db|
+      db.execute("INSERT INTO lyc ( lycname , album , albumLink , artist , artistLink , lyccontent ) VALUES ('#{@data["_lrcname"]}' , '#{@data["_album"]}' , '#{@data["_albumLink"]}' , '#{@data["_artist"]}' , '#{@data["_artistLink"]}', '#{@data["_lyccontent"]}')")
+      db.close
+    end
+  end
+
+
   # url 字母链接
   # database 字母(和数据库名字相关)： a b c ...
   
-    @nowarray  = []
-    @alllist = []
-    @database = 1
-    $queue = []
+    
     28.times.each_slice(3) do |i|
       @alllist<<i
     end
@@ -26,21 +45,14 @@ require 'enumerator'
         # puts Thread.list.length()
         @nowarray<< Thread.new do
             xid = $queue.pop()
+            insert xid
         end
       end
       @nowarray.each do |t|
         t.join
       end
     end
- 
-  def insert    
-        
-        # # 根据@database的名字不同 存入不同的数据库
-        # SQLite3::Database.new("z.db") do |db|
-        #   db.execute("INSERT INTO lyc ( lycname , album , albumLink , artist , artistLink , lyccontent ) VALUES ('#{@data["_lrcname"]}' , '#{@data["_album"]}' , '#{@data["_albumLink"]}' , '#{@data["_artist"]}' , '#{@data["_artistLink"]}', '#{lydata}')")
-        #   db.close
-        # end
-  end
+
 
 
 
@@ -49,6 +61,3 @@ require 'enumerator'
 # 还差一个 qita 分类没有下载 用run = Getlrc.new("http://www.kuwo.cn/geci/artist_qita.htm")
 
 # 根据不同的字母 存入不同的数据库
-
-  puts " -----1组开始------- "
-  insert 
